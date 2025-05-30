575a56d05ff6e33381db9dd3023049587e81e3c3
yasindu2000 <yasindudahanayake@gmail.com>
2025-05-30 23:14:12 +0530

    updates

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,28 @@
-console.log("hello world")
+import express from "express";
+
+let app = express();
+
+app.get("/", (req,res)=>{
+    
+    res.json({
+        message : "this is a get request",
+    })
+})
+
+app.post("/", (req,res)=>{
+
+     res.json({
+        message : "this is a post request",
+    })
+})
+
+app.delete("/", (req,res)=>{
+     res.json({
+        message : "this is a delete request",
+    })
+})
+
+
+app.listen(5000, ()=>{
+    console.log("Server Is Running");
+})
