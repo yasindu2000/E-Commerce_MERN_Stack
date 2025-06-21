3a8a34d432d80dbc13e973576e9e46eb7c524d08
yasindu2000 <yasindudahanayake@gmail.com>
2025-06-21 14:18:03 +0530

    update model

--- backend/model/product.js
+++ backend/model/product.js
@@ -1,7 +1,50 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema(
-    {
-        
+const productSchema = new mongoose.Schema({
+
+    productId : {
+        type : String,
+        required : true,
+        unique : true
+    },
+
+    name : {
+        type : String,
+        required : true
+    },
+    altNames : {
+        type : [String],
+        default : []
+    },
+    labelledPrice : {
+        type : Number,
+        required : true
+    },
+    price : {
+        type : Number,
+        required : true
+    },
+    images : {
+        type : [String],
+        default : ["/degault-product.jpg"]
+    },
+    description : {
+        type : String,
+        required : true
+    },
+    stock : {
+        type : Number,
+        required : true,
+        default : 0
+    },
+    isAvailble : {
+        type : Boolean,
+        default : true
+    },
+    category : {
+        type : String,
+        required : true,
+        default : "cosmatics"
     }
-)
+
+})
