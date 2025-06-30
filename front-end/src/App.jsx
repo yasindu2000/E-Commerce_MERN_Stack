8134311f3ce28bbbb5532d6a2596b9ce76f0f33c
yasindu2000 <yasindudahanayake@gmail.com>
2025-06-30 12:04:47 +0530

    update🚀

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,8 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom"
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
+import Register from "./pages/register"
 import Admin from "./pages/Admin"
-
+import Login from "./pages/Login"
+import Home from './pages/Home'
 
 function App() {
   
--- front-end/src/pages/Admin.jsx
+++ front-end/src/pages/Admin.jsx
@@ -1,24 +1,30 @@
 
-import { Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes } from "react-router-dom";
+import { FaBoxArchive } from "react-icons/fa6";
+import { GiShoppingBag } from "react-icons/gi";
+import { IoPeople } from "react-icons/io5";
+import { IoSettings } from "react-icons/io5";
 
 function Admin() {
   return (
-    <div className='w-full h-screen bg-red-500 flex'>
-      <div className="w-[300px] h-full bg-amber-50">
-    
-      </div>
-      <div className="w-[calc(100%-300px)] h-full bg-blue-500">
-         <Routes path='/'>
-              
-              <Route path='/' element ={<h1>Dahboard</h1>}/>
-              <Route path='/products' element={<h1>Products</h1>}/>
-              <Route path='/orders' element={<h1>Orders</h1>}/>
+      <div className="w-full h-screen  flex">
+            <div className="w-[300px] h-full flex flex-col items-center">
+                <span className="text-3xl font-bold my-5">Admin Panel</span>
 
-         </Routes>
-      </div>
-
-
-    </div>
+                <Link className="flex flex-row h-[60px] w-full  border p-[20px] items-center text-xl  gap-[25px]" to="/admin/products"><FaBoxArchive /> Products</Link>
+                <Link className="flex flex-row h-[60px] w-full border p-[20px] items-center text-xl  gap-[25px]" to="/admin/orders"><GiShoppingBag /> Orders</Link>
+                <Link className="flex flex-row h-[60px] w-full border p-[20px] items-center text-xl  gap-[25px]" to="/admin/users"><IoPeople /> Users</Link>
+                <Link className="flex flex-row h-[60px] w-full border p-[20px] items-center text-xl  gap-[25px]" to="/admin/settings"><IoSettings /> Settings</Link>
+            </div>
+            <div className="w-[calc(100%-300px)]  h-full">
+                <Routes path="/*">
+                    <Route path="/" element={<h1>Dashboard</h1>}/>
+                    <Route path="/products" element={<h1>Products</h1>}/>
+                    <Route path="/orders" element={<h1>Orders</h1>}/>
+                </Routes>
+            </div>
+            
+        </div>
   )
 }
 
