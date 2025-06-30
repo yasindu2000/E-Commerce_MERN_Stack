a3c625b708e64cdf678e1e690266e880d70a3cf6
yasindu2000 <yasindudahanayake@gmail.com>
2025-06-30 23:15:02 +0530

    create login page and connect backend🚀

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,13 @@ import userRouter from "./routers/userRouter.js";
 import jwt from "jsonwebtoken"
 import productRouter from "./routers/productRouter.js";
 import dotenv from 'dotenv'
+import cors from "cors"
 dotenv.config();
 
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(
     (req, res, next)=>{
--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,13 +1,49 @@
-import React from 'react'
+import { useState } from 'react'
+import { Link } from "react-router-dom";
+import axios from "axios";
+
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = ()=>{
+    console.log(email, password);
+    axios.post("http://localhost:5000/users/login",{
+      email : email,
+      password : password
+    }).then((response)=>{
+               console.log(response.data)
+    }).catch((error)=>{
+                console.log(error)
+    })
+  }
   return (
-    <div className='w-full h-screen bg-[url(./login.jpg)] bg-cover bg-center flex justify-center items-center'>
-      <div className="w-[500px] h-[500px] backdrop-blur-sm shadow-2xl rounded-[30px]">
-          <h1 className='text-3xl font-bold  text-center my-5 text-white'>Login</h1>
-          <input type="text" className="" />
-      </div>
-    </div>
+    <div className="w-full h-screen bg-[url(./login.jpg)] bg-cover bg-center flex justify-center items-center">
+			<div className="w-[500px] h-[500px] backdrop-blur-sm shadow-2xl rounded-[30px] relative gap-[20px] text-white flex flex-col items-center justify-center">
+				<h1 className="absolute top-[20px] text-2xl font-bold text-center my-5">Login</h1>
+                <div className="w-[350px]  flex flex-col">
+                    <span className="text-lg ">Email</span>
+                    <input onChange={(e)=>{
+                      setEmail(e.target.value)
+
+                    }} type="text" className="w-[350px] h-[40px] border border-white rounded-xl"/>
+                </div>
+                <div className="w-[350px]  flex flex-col">
+                    <span className="text-lg ">Password</span>
+
+                    <input onChange={(e)=>{
+                          setPassword(e.target.value)
+                    }} type="password" className="w-[350px] h-[40px] border border-white rounded-xl"/>
+
+                </div>
+                <button onClick={handleSubmit} className="w-[350px] h-[40px] bg-blue-500 rounded-xl text-white text-lg mt-5 hover:bg-blue-600 transition-all duration-300 cursor-pointer">
+                    Login
+                </button>
+                <p>Don't have an account? <Link to="/register" className="text-blue-500">Sign up</Link> from here</p>
+                
+			</div>
+		</div>
   )
 }
 
